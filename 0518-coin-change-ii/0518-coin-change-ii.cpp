712dfa43ac9048bafea9b8d173ce7ffdaae08ae1
krish <61118591+krish717@users.noreply.github.com>
2022-11-08 17:59:08 +0530

    Time: 103 ms (24.67%), Space: 18.3 MB (56.61%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -13,8 +13,28 @@ public:
                 return dp[ind][tar] =  nottake + take;
         }
     int change(int amount, vector<int>& coins) {
+            // int n = coins.size();
+            // vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+        // return helper(n-1,amount,coins,dp);
+            
+            //Tabulation 
             int n = coins.size();
-            vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-        return helper(n-1,amount,coins,dp);
+            vector<vector<int>> dp(n,vector<int>(amount+1,0));
+            //base casew
+            for(int tar=0; tar<=amount; tar++){
+                    dp[0][tar] = (tar%coins[0]==0);
+            }
+            for(int ind=1; ind<n; ind++){
+                    for(int tar=0; tar<=amount; tar++){
+                           int nottake = dp[ind-1][tar];
+              int take = 0;
+                if(coins[ind]<=tar){
+                        take = dp[ind][tar-coins[ind]];
+                }
+                 dp[ind][tar] =  nottake + take;  
+                    }
+            }
+            
+            return dp[n-1][amount];
     }
 };
