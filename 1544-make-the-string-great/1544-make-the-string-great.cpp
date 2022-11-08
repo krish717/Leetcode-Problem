27ccdc8c0dc588e9292ca2c7aefdc5e18ae190a2
krish <61118591+krish717@users.noreply.github.com>
2022-11-08 11:44:41 +0530

    Time: 11 ms (14.85%), Space: 7.2 MB (8.20%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -1,23 +1,33 @@
 class Solution {
 public:
     string makeGood(string s) {
-        stack<char> st;
-            for(int i=0; i<s.size(); i++){
-                    if(st.empty()){
-                            st.push(s[i]);
-                            continue;
-                    }
-                    if(abs(st.top()-s[i])==32){
-                            st.pop();
-                    }else{
-                         st.push(s[i]);   
+        // stack<char> st;
+        //     for(int i=0; i<s.size(); i++){
+        //             if(st.empty()){
+        //                     st.push(s[i]);
+        //                     continue;
+        //             }
+        //             if(abs(st.top()-s[i])==32){
+        //                     st.pop();
+        //             }else{
+        //                  st.push(s[i]);   
+        //             }
+        //     }
+        //     string ans = "";
+        //     while(!st.empty()){
+        //             ans=st.top()+ans;
+        //             st.pop();
+        //     }
+        //     return ans;
+            int st = 0;
+            while(st!=s.size()){
+                    st = s.size();
+                    for(int i=0; i<s.size(); i++){
+                            if(abs(s[i]-s[i+1])==32){
+                                    s = s.substr(0,i) + s.substr(i+2);
+                            }
                     }
             }
-            string ans = "";
-            while(!st.empty()){
-                    ans=st.top()+ans;
-                    st.pop();
-            }
-            return ans;
+            return s;
     }
 };
