6377c84e704f76dca67221e7a41da413077f7afb
krish <61118591+krish717@users.noreply.github.com>
2022-11-11 13:16:25 +0530

    Time: 34 ms (64.87%), Space: 13.1 MB (24.65%) - LeetHub

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+        int helper(int n,int m,string text1, string text2,vector<vector<int>>& dp){
+               if(n<0 || m<0) return 0;
+                if(dp[n][m]!=-1) return dp[n][m];
+                if(text1[n]==text2[m]) return dp[n][m] = 1+helper(n-1,m-1,text1,text2,dp);
+                return dp[n][m] = max(helper(n-1,m,text1,text2,dp),helper(n,m-1,text1,text2,dp));
+        }
+    int longestCommonSubsequence(string text1, string text2) {
+            int n=text1.size();
+            int m=text2.size();
+            vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+            for(int i=0; i<=n; i++) dp[i][0] = 0;
+            for(int j=0; j<=m; j++) dp[0][j] = 0;
+        // return helper(n-1,m-1,text1,text2,dp);
+            //Tabulation 
+            for(int i=1; i<=n; i++){
+                    for(int j=1; j<=m; j++){
+                         if(text1[i-1]==text2[j-1])  dp[i][j] = 1+dp[i-1][j-1];
+                         else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);   
+                    }
+            }
+            return dp[n][m];
+    }
+};
