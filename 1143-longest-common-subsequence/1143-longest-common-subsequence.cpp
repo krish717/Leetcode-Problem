4ddfd374fc5a446d5012b367c886641c7ba6cad6
krish <61118591+krish717@users.noreply.github.com>
2022-11-11 13:23:17 +0530

    Time: 28 ms (74.36%), Space: 12.8 MB (64.26%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -25,17 +25,17 @@ public:
             
             //Space optimized
             vector<int> prev(m+1,0);
-            vector<int> curr(m+1,0);
+         
              for(int i=0; i<=n; i++) prev[0] = 0;
             // for(int j=0; j<=m; j++) dp[0][j] = 0;
             for(int i=1; i<=n; i++){
-                    
+                       vector<int> curr(m+1,0);
                     for(int j=1; j<=m; j++){
                          if(text1[i-1]==text2[j-1])  curr[j] = 1+prev[j-1];
                          else curr[j] = max(prev[j],curr[j-1]);   
                     }
                     prev = curr;
             }
-            return curr[m];
+            return prev[m];
     }
 };
