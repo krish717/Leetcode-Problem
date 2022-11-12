8fb74d6be510fbe1721130bc3ba7783b1b6163cd
krish <61118591+krish717@users.noreply.github.com>
2022-11-12 14:55:19 +0530

    Time: 20 ms (78.98%), Space: 12.9 MB (39.36%) - LeetHub

---
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    string shortestCommonSupersequence(string text1, string text2) {
+        int n=text1.size();
+            int m=text2.size();
+            vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+            for(int i=0; i<=n; i++) dp[i][0] = 0;
+            for(int j=0; j<=m; j++) dp[0][j] = 0;
+     
+            
+            for(int i=1; i<=n; i++){
+                    for(int j=1; j<=m; j++){
+                         if(text1[i-1]==text2[j-1])  dp[i][j] = 1+dp[i-1][j-1];
+                         else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);   
+                    }
+            }
+           
+            
+           
+            string ans = "";
+                    
+           
+         
+            int i=n,j=m;
+            while(i>0 && j>0){
+                    if(text1[i-1]==text2[j-1]){
+                            ans.push_back(text1[i-1]);
+                           
+                            i--;
+                            j--;
+                    }else if(dp[i-1][j]>dp[i][j-1]){
+                            ans.push_back(text1[i-1]);
+                            i--;
+                    }else{
+                            ans.push_back(text2[j-1]);
+                            j--;
+                    }
+            }
+             while(i>0) // if s1 characters are still left
+    {
+        ans.push_back(text1[i-1]);
+        i--;
+    }
+    while(j>0) //if s2 characters are still left
+    {
+        ans.push_back(text2[j-1]);
+        j--;
+        
+    }
+        reverse(ans.begin(),ans.end()); 
+            return ans;
+    }
+};
