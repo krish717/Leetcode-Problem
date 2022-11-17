4e7228d5740c1a9f36f03d62426fc16c606bbc9e
krish <61118591+krish717@users.noreply.github.com>
2022-11-18 00:52:26 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (82.94%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -7,15 +7,17 @@ public:
         // }
     int climbStairs(int n) {
              if(n==1 || n==2) return n;
-        vector<int> dp(n+1,0);
+        // vector<int> dp(n+1,0);
             // return helper(n,dp);
-            dp[1] = 1;
-            dp[2] = 2;
+           int prev1 = 1;
+           int prev2 = 1;
            
             //Tabulation
-            for(int i=3; i<=n; i++){
-                    dp[i] = dp[i-1] + dp[i-2];
+            for(int i=2; i<=n; i++){
+                   int curr = prev1+prev2;
+                    prev2 = prev1;
+                    prev1 = curr;
             }   
-            return dp[n];
+            return prev1;
     }
 };
