99c0e9032bdab4b63c7ee26911d709cd4d11908d
krish <61118591+krish717@users.noreply.github.com>
2022-11-18 14:02:13 +0530

    Time: 10 ms (73.68%), Space: 13.9 MB (12.42%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-//         int helper(vector<int>& cost,int ind,int n, vector<int>& dp){
-                
-//                 if(ind>=cost.size()) return 0;
-//                 if(dp[ind]!=-1) return dp[ind];
-//                 int pick =    cost[ind]+ helper(cost,ind+1,n,dp);
-//                 int notpick =  cost[ind]+helper(cost,ind+2,n,dp);
-//                 return dp[ind] =   min(pick, notpick);
-//         }
+        int helper(vector<int>& cost,int n, vector<int>& dp){
+                if(n<0) return 0;
+                if(n==0 || n==1) return cost[n];
+                if(dp[n]!=-1) return dp[n];
+                int pick =    cost[n] + helper(cost,n-1,dp);
+                int notpick = cost[n] + helper(cost,n-2,dp);
+                return dp[n] =   min(pick, notpick);
+        }
     int minCostClimbingStairs(vector<int>& cost) {
             int n = cost.size();
-            int ind=0;
-            vector<int> dp(n+1);
+            
+            vector<int> dp(n+1,-1);
         
-           // return min(helper(cost,ind,n,dp), helper(cost,ind+1,n,dp));
+           return min(helper(cost,n-1,dp), helper(cost,n-2,dp));
             
 //             // Tabulation
-            dp[0] = cost[0];
-            dp[1] = cost[1];
-            for(int i=2; i<n; i++){
-                int pick = dp[i-1];
-                int notpick = dp[i-2];
-                 dp[i] = cost[i] + min(pick, notpick);
-            }
-            return min(dp[n-1],dp[n-2]);
+            // dp[0] = cost[0];
+            // dp[1] = cost[1];
+            // for(int i=2; i<n; i++){
+            //     int pick = dp[i-1];
+            //     int notpick = dp[i-2];
+            //      dp[i] = cost[i] + min(pick, notpick);
+            // }
+            // return min(dp[n-1],dp[n-2]);
     }
 };
