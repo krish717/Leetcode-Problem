5542d23368dc489fe7509a246901ac86d5859147
krish <61118591+krish717@users.noreply.github.com>
2022-11-18 13:29:33 +0530

    Time: 10 ms (73.68%), Space: 13.9 MB (30.93%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
-        int helper(vector<int>& cost,int n, vector<int>& dp){
-                if(n<0) return 0;
-                if(n==0 || n==1) return cost[n];
-                if(dp[n]!=-1) return dp[n];
-                int ans1 = helper(cost,n-1,dp);
-                int ans2 = helper(cost,n-2,dp);
-                return dp[n] = cost[n] + min(ans1, ans2);
+        int helper(vector<int>& cost,int ind,int n, vector<int>& dp){
+                
+                if(ind>=cost.size()) return 0;
+                if(dp[ind]!=-1) return dp[ind];
+                int pick =     helper(cost,ind+1,n,dp);
+                int notpick =  helper(cost,ind+2,n,dp);
+                return dp[ind] =  cost[ind] + min(pick, notpick);
         }
     int minCostClimbingStairs(vector<int>& cost) {
             int n = cost.size();
+            int ind=0;
             vector<int> dp(n+1,-1);
         
-            return min(helper(cost,n-1,dp), helper(cost,n-2,dp));
+           return min(helper(cost,ind,n,dp), helper(cost,ind+1,n,dp));
+            
+//             // Tabulation
+//             dp[0] = cost[0];
+//             dp[1] = cost[1];
+//             for(int i=2; i<=n; i++){
+//                 int pick = dp[n-1] + cost[i-1];
+//                 int notpick = dp[n-2] + cost[i-2];
+//                  dp[i] = min(pick, notpick);
+//             }
+//             return dp[n];
     }
 };
