ddf35cbda502dc2fa2e7ea3c6130e6bf3e595020
krish <61118591+krish717@users.noreply.github.com>
2022-11-18 13:57:47 +0530

    Time: 3 ms (98.63%), Space: 13.8 MB (30.93%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-        int helper(vector<int>& cost,int ind,int n, vector<int>& dp){
+//         int helper(vector<int>& cost,int ind,int n, vector<int>& dp){
                 
-                if(ind>=cost.size()) return 0;
-                if(dp[ind]!=-1) return dp[ind];
-                int pick =    cost[ind]+ helper(cost,ind+1,n,dp);
-                int notpick =  cost[ind]+helper(cost,ind+2,n,dp);
-                return dp[ind] =   min(pick, notpick);
-        }
+//                 if(ind>=cost.size()) return 0;
+//                 if(dp[ind]!=-1) return dp[ind];
+//                 int pick =    cost[ind]+ helper(cost,ind+1,n,dp);
+//                 int notpick =  cost[ind]+helper(cost,ind+2,n,dp);
+//                 return dp[ind] =   min(pick, notpick);
+//         }
     int minCostClimbingStairs(vector<int>& cost) {
             int n = cost.size();
             int ind=0;
-            vector<int> dp(n+1,-1);
+            vector<int> dp(n+1);
         
-           return min(helper(cost,ind,n,dp), helper(cost,ind+1,n,dp));
+           // return min(helper(cost,ind,n,dp), helper(cost,ind+1,n,dp));
             
 //             // Tabulation
-//             dp[0] = cost[0];
-//             dp[1] = cost[1];
-//             for(int i=2; i<=n; i++){
-//                 int pick = dp[n-1] + cost[i-1];
-//                 int notpick = dp[n-2] + cost[i-2];
-//                  dp[i] = min(pick, notpick);
-//             }
-//             return dp[n];
+            dp[0] = cost[0];
+            dp[1] = cost[1];
+            for(int i=2; i<n; i++){
+                int pick = dp[i-1];
+                int notpick = dp[i-2];
+                 dp[i] = cost[i] + min(pick, notpick);
+            }
+            return min(dp[n-1],dp[n-2]);
     }
 };
