6b4f6924bf1c7e6cc8a9438b74f00f3f5bc93d0c
krish <61118591+krish717@users.noreply.github.com>
2022-12-05 21:58:29 +0530

    Time: 14 ms (5.37%), Space: 6.5 MB (95.12%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     bool halvesAreAlike(string s) {
-        int cn1 = 0, cn2 = 0;
-        for(int i=0;i<s.size()/2;i++){
-            char ch = s[i];
-            if(ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u') cn1++;
-            else if(ch == 'A' or ch == 'E' or ch == 'I' or ch == 'O' or ch == 'U') cn1++;
-        }
-        for(int i=s.size()/2;i<s.size();i++){
-            char ch = s[i];
-            if(ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u') cn2++;
-            else if(ch == 'A' or ch == 'E' or ch == 'I' or ch == 'O' or ch == 'U') cn2++;
-        }
-        return cn1 == cn2;
+        int count1 = 0;
+        int count2 = 0;
+            for(int i=0; i<s.size()/2; i++){
+               char ch = s[i];
+               if(ch=='a' || ch=='e' || ch=='i' || ch=='o' ||ch=='u') count1++;
+               else if(ch=='A' || ch=='E' || ch=='I' || ch=='O' ||ch=='U') count1++;  
+            }
+            
+             for(int i=s.size()/2; i<s.size(); i++){
+               char ch = s[i];
+               if(ch=='a' || ch=='e' || ch=='i' || ch=='o' ||ch=='u') count2++;
+               else if(ch=='A' || ch=='E' || ch=='I' || ch=='O' ||ch=='U') count2++;  
+            }
+            
+            return (count1==count2);
     }
 };
