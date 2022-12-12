74862823e35236dfc90372c485e256f64244c37b
krish <61118591+krish717@users.noreply.github.com>
2022-12-12 20:52:26 +0530

    Time: 7 ms (99.00%), Space: 21 MB (27.02%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -14,9 +14,8 @@ public:
         int check(TreeNode* root){
                 if(root==NULL) return 0;
                 int lh = check(root->left);
-                if(lh==-1) return -1;
                 int rh = check(root->right);
-                 if(rh==-1) return -1;
+                if(lh==-1 || rh==-1) return -1;
                 if(abs(lh-rh)>1) return -1;
                 return max(lh,rh)+1;
         }
