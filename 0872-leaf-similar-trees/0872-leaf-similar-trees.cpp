fa09bd2f1c58c3c8f5ba7b3bd2060211c9b67fec
krish <61118591+krish717@users.noreply.github.com>
2022-12-12 15:44:05 +0530

    Time: 7 ms (54.91%), Space: 12.9 MB (41.32%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
@@ -13,18 +13,15 @@ class Solution {
 public:
         void leafSimilar(TreeNode* root,vector<int>& v){
                 if(root==NULL) return;
-                if(root->left==NULL && root->right==NULL){
-                        v.push_back(root->val);
-                }
-                leafSimilar(root->left,v);
-                leafSimilar(root->right,v);
+        if((root->left==NULL && root->right==NULL)) v.push_back(root->val);
+             leafSimilar(root->left,v);
+             leafSimilar(root->right,v);   
         }
     bool leafSimilar(TreeNode* root1, TreeNode* root2) {
-            if(root1==NULL && root2==NULL) return true;
-            vector<int> v1;
-            vector<int> v2;
-            leafSimilar(root1,v1);
-            leafSimilar(root2,v2);
-            return v1==v2;
+        vector<int> v1;
+        vector<int> v2;
+        leafSimilar(root1,v1);
+        leafSimilar(root2,v2);    
+            return (v1==v2);
     }
 };
