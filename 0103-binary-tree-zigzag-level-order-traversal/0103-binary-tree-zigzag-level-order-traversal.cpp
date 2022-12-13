6775b0c37260e605fbca8ed27ff4f5e59bb0f0c7
krish <61118591+krish717@users.noreply.github.com>
2022-12-13 23:13:28 +0530

    Time: 7 ms (45.45%), Space: 11.9 MB (99.14%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -12,37 +12,27 @@
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-vector<vector<int>> ans;
-if(root==NULL) return ans;
-stack<TreeNode*> s1;
-stack<TreeNode*> s2;
-s1.push(root);
-int flag = 1;
-while(!s1.empty() || !s2.empty()){
-vector<int> v;
-if(flag==1){
-int size = s1.size();
-for(int i=0; i<size; i++){
-TreeNode* temp = s1.top();
-s1.pop();
-v.push_back(temp->val);
-if(temp->left!=NULL) s2.push(temp->left); 
-if(temp->right!=NULL) s2.push(temp->right);
-}
-if(s1.empty()) flag=0;
-}else if(flag==0){
-int size = s2.size();
-for(int i=0; i<size; i++){
-TreeNode* temp = s2.top();
-s2.pop();
-v.push_back(temp->val);
-if(temp->right!=NULL) s1.push(temp->right); 
-if(temp->left!=NULL) s1.push(temp->left);
-}
-if(s2.empty()) flag=1;
-}
-ans.push_back(v);
-}
-return ans;
+         vector<vector<int>> ans;
+          if(root==NULL) return ans;
+            queue<TreeNode*> q;
+            q.push(root);
+            bool LefttoRight = true;
+            while(!q.empty()){
+                    int size = q.size();
+                    vector<int> v(size);
+                    for(int i=0; i<size; i++){
+                            TreeNode *temp = q.front();
+                            q.pop();
+                            //find position to fill value;
+                            int index = LefttoRight ? i : (size-i-1);
+                            v[index] = temp->val;
+                            if(temp->left!=NULL) q.push(temp->left);
+                            if(temp->right!=NULL) q.push(temp->right);
+                    }
+                    //update the leve;
+                    LefttoRight=!LefttoRight;
+                    ans.push_back(v);
+            }
+            return ans;
     }
 };
