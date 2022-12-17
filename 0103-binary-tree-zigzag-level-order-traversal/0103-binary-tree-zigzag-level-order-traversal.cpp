4bca1d857d78aa60d8d0177a605cd34417672393
krish <61118591+krish717@users.noreply.github.com>
2022-12-17 19:16:07 +0530

    Time: 4 ms (64.24%), Space: 12.2 MB (45.35%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -12,36 +12,24 @@
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-            vector<vector<int>> ans;
+        vector<vector<int>> ans;
             if(root==NULL) return ans;
-        stack<TreeNode *> s1;
-        stack<TreeNode *> s2;
-            int flag=1;
-            s1.push(root);
-            while(!s1.empty() || !s2.empty()){
-                    vector<int> temp;
-                  if(flag==1){
-                          int size = s1.size();
-                          for(int i=0; i<size; i++){
-                                  root = s1.top();
-                                  s1.pop();
-                                  temp.push_back(root->val);
-                                  if(root->left) s2.push(root->left);
-                                  if(root->right) s2.push(root->right);
-                          }
-                          if(s1.empty()) flag=0;
-                  }else{
-                         int size = s2.size();
-                          for(int i=0; i<size; i++){
-                                  root = s2.top();
-                                  s2.pop();
-                                  temp.push_back(root->val);
-                                  if(root->right) s1.push(root->right);
-                                  if(root->left) s1.push(root->left);
-                                  
-                          }
-                          if(s2.empty()) flag=1;  
-                  }
+            queue<TreeNode *> q;
+            q.push(root);
+            bool flag=true;
+            while(!q.empty()){
+                    int size = q.size();
+                    vector<int> temp(size);
+                    for(int i=0; i<size; i++){
+                            root = q.front();
+                            q.pop();
+                          
+                            int index = flag ? i : (size-i-1);
+                            temp[index] = (root->val);
+                             if(root->left) q.push(root->left);
+                             if(root->right) q.push(root->right);      
+                    }
+                    flag = !flag;
                     ans.push_back(temp);
             }
             return ans;
