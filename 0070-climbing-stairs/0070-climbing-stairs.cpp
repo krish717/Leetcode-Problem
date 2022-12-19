f03f4061ab343d402082bb223684b938d80f51a8
krish <61118591+krish717@users.noreply.github.com>
2022-12-19 06:47:56 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (29.22%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,23 +1,12 @@
 class Solution {
 public:
-        // int helper(int ind,vector<int>&dp){
-        //         if(ind==1 || ind==2) return ind;
-        //         if(dp[ind]!=-1) return dp[ind];
-        //     return dp[ind] = helper(ind-1,dp) + helper(ind-2,dp); 
-        // }
+        int fun(int ind,vector<int>& dp){
+               if(ind==1 || ind==2) return ind;
+                if(dp[ind]!=-1) return dp[ind];
+                return dp[ind] = fun(ind-1,dp) + fun(ind-2,dp);
+        }
     int climbStairs(int n) {
-             if(n==1 || n==2) return n;
-        // vector<int> dp(n+1,0);
-            // return helper(n,dp);
-           int prev1 = 1;
-           int prev2 = 1;
-           
-            //Tabulation
-            for(int i=2; i<=n; i++){
-                   int curr = prev1+prev2;
-                    prev2 = prev1;
-                    prev1 = curr;
-            }   
-            return prev1;
+        vector<int> dp(n+1,-1);
+            return fun(n,dp);
     }
 };
