6751e98ff68bcf2fd11abfc1303a81c58dd01bfa
krish <61118591+krish717@users.noreply.github.com>
2022-12-21 20:41:11 +0530

    Time: 5 ms (19.77%), Space: 6.3 MB (60.63%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -9,22 +9,24 @@ public:
         //         return dp[i][j] = pick + notpick;
         // } 
     int uniquePaths(int m, int n) {
-            vector<vector<int>> dp(m,vector<int>(n));
+            vector<int> prev(n,0);
            
             for(int i=0; i<m; i++){
+                    vector<int> curr(n,0);
                     for(int j=0; j<n; j++){
-                            if(i==0 && j==0)  dp[0][0]=1;
+                            if(i==0 && j==0)  curr[0]=1;
                             else{
                                   int up=0,left=0;
                                   if(i>0)
-                                  up = dp[i-1][j];
+                                  up = prev[j];
                                   if(j>0)
-                                  left = dp[i][j-1];
-                                  dp[i][j] = up + left;    
+                                  left = curr[j-1];
+                                  curr[j] = up + left;    
                             }
                             
                     }
+                    prev = curr;
             }
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 };
