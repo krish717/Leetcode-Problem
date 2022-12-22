4720e8d71d11bf627fa4ac8bd830411fb17ef32c
krish <61118591+krish717@users.noreply.github.com>
2022-12-22 09:32:02 +0530

    Time: 8 ms (77.88%), Space: 9.1 MB (33.42%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
-        int fun(int i,int j,int n,vector<vector<int>>& triangle,vector<vector<int>>& dp){
-                if(i==n-1) return triangle[n-1][j];
-                if(dp[i][j]!=-1) return dp[i][j];
-                int ans1 = triangle[i][j] + fun(i+1,j,n,triangle,dp);
-                int ans2 = triangle[i][j] + fun(i+1,j+1,n,triangle,dp);
-                return dp[i][j] = min(ans1,ans2);
-        }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-         
-        vector<vector<int>> dp(n,vector<int>(n,-1));
-        return fun(0,0,n,triangle,dp);    
+        vector<vector<int>> dp(n,vector<int>(n,0));
+            //base case
+            for(int j=0; j<n; j++){
+                    dp[n-1][j] = triangle[n-1][j]; 
+            }
+            
+            for(int i=n-2; i>=0; i--){
+                    for(int j=i; j>=0; j--){
+                            int ans1 = triangle[i][j] + dp[i+1][j];
+                            int ans2 = triangle[i][j] + dp[i+1][j+1];
+                            dp[i][j] = min(ans1,ans2);
+                    }
+            }
+            return dp[0][0];
     }
 };
