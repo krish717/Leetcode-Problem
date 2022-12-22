4f2b261d5eaf13ac264bfe4f1c6d8bf1d2fd6d85
krish <61118591+krish717@users.noreply.github.com>
2022-12-22 11:27:24 +0530

    Time: 14 ms (90.10%), Space: 10.3 MB (37.08%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,42 +1,35 @@
 class Solution {
 public:
-        // int fun(int i,int j,vector<vector<int>>& matrix,vector<vector<int>>& dp){
-        //         if(j<0 || j>=matrix[0].size()) return 1e9;
-        //         if(i==0) return matrix[0][j];
-        //         if(dp[i][j]!=-1) return dp[i][j];
-        //         int up = matrix[i][j] + fun(i-1,j,matrix,dp);
-        //         int ld = matrix[i][j] + fun(i-1,j-1,matrix,dp);
-        //         int rd = matrix[i][j] + fun(i-1,j+1,matrix,dp);
-        //         return dp[i][j] = min(up,min(ld,rd));
-        // }
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-            vector<vector<int>> dp(n,vector<int>(m,0));
+            vector<int> prev(n);
             
             
             //base case
             for(int j=0; j<m; j++){
-                    dp[0][j] = matrix[0][j];
+                    prev[j] = matrix[0][j];
             }
 
             
             for(int i=1; i<n; i++){
+                    vector<int> curr(m);
                     for(int j=0; j<m; j++){
-                         int up = matrix[i][j] + dp[i-1][j];
+                         int up = matrix[i][j] + prev[j];
                          int ld = matrix[i][j];
-                            if(j-1>=0) ld+=dp[i-1][j-1];
+                            if(j-1>=0) ld+=prev[j-1];
                             else ld += 1e9;
                          int rd = matrix[i][j];
-                            if(j+1<m) rd+=dp[i-1][j+1];
+                            if(j+1<m) rd+=prev[j+1];
                             else rd +=1e9; 
-                         dp[i][j] = min(up,min(ld,rd));   
+                         curr[j] = min(up,min(ld,rd));   
                     }
+                    prev = curr;
             }
             
             int maxi = 1e9;
             for(int j=0; j<m; j++){
-                    maxi = min(maxi,dp[n-1][j]);
+                    maxi = min(maxi,prev[j]);
             }
             return maxi;
     }
