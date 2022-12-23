cc51c50c0a37ef7b7159d46eff10a680bedc3d79
krish <61118591+krish717@users.noreply.github.com>
2022-12-23 20:08:10 +0530

    Time: 0 ms (100.00%), Space: 11.1 MB (45.36%) - LeetHub

--- 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.cpp
+++ 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.cpp
@@ -11,22 +11,18 @@
  */
 class Solution {
 public:
-       
-        void fun(TreeNode* root, int x, int y,int& depth1,int& depth2,int count){
+        void fun(TreeNode* root, int x, int y,int& ans1,int &ans2,int count){
                 if(root==NULL) return;
-                
-                if(root->val==x) depth1 = count;
-                 if(root->val==y)  depth2 = count;
+                if(root->val==x) ans1 = count;
+                if(root->val==y) ans2 = count;
                 if(root->left && root->right && root->left->val==x && root->right->val==y) return;
                 if(root->left && root->right && root->left->val==y && root->right->val==x) return;
-                 fun(root->left,x,y,depth1,depth2,count+1);
-                 fun(root->right,x,y,depth1,depth2,count+1);               
+                fun(root->left,x,y,ans1,ans2,count+1);
+                fun(root->right,x,y,ans1,ans2,count+1);
         }
     bool isCousins(TreeNode* root, int x, int y) {
-        int depth1=-1,depth2=-2;
-            
-            if(root->val==x || root->val==y) return false;
-         fun(root,x,y,depth1,depth2,0);
-           return (depth1==depth2);
+            int ans1 = -1,ans2=-2;
+            fun(root,x,y,ans1,ans2,0);
+            return (ans1==ans2);
     }
 };
