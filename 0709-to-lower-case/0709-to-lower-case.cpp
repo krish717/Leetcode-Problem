88bae6697629d6cfd6cdd148b5850a3e1c1f87fb
krish <61118591+krish717@users.noreply.github.com>
2023-01-02 14:35:52 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (56.44%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.cpp
+++ 0709-to-lower-case/0709-to-lower-case.cpp
@@ -1,10 +1,7 @@
 class Solution {
 public:
     string toLowerCase(string s) {
-            string s1 = "";
-        for(int i=0; i<s.size(); i++){
-               s1+=tolower(s[i]);
-        }
-            return s1;
+        transform(s.begin(),s.end(),s.begin(),::tolower);
+            return s;
     }
 };
