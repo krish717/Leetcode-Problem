08f2c4445ab84f9e8215cb3ba66043111ced4983
krish <61118591+krish717@users.noreply.github.com>
2023-01-02 15:17:07 +0530

    Time: 7 ms (97.49%), Space: 18.6 MB (33.02%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,40 +1,20 @@
 class Solution {
 public:
-        int helper(int ind,int tar,vector<int>& coins, vector<vector<int>>& dp){
-              if(ind==0){
-                      return (tar%coins[0]==0);
-              }  
-               if(dp[ind][tar]!=-1) return dp[ind][tar]; 
-              int nottake = helper(ind-1,tar,coins,dp);
-              int take = 0;
-                if(coins[ind]<=tar){
-                        take = helper(ind,tar-coins[ind],coins,dp);
+        int fun(int ind,int amount, vector<int>& coins,vector<vector<int>> &dp){
+                if(ind==0){
+                     return (amount%coins[0]==0);
                 }
-                return dp[ind][tar] =  nottake + take;
+                if(dp[ind][amount]!=-1) return dp[ind][amount];
+                int notpick = fun(ind-1,amount,coins,dp);
+                int pick = 0;
+                if(coins[ind]<=amount){
+                      pick =  fun(ind,amount-coins[ind],coins,dp);  
+                }
+                return dp[ind][amount] = pick + notpick;
         }
     int change(int amount, vector<int>& coins) {
-            // int n = coins.size();
-            // vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-        // return helper(n-1,amount,coins,dp);
-            
-            //Tabulation 
             int n = coins.size();
-            vector<vector<int>> dp(n,vector<int>(amount+1,0));
-            //base casew
-            for(int tar=0; tar<=amount; tar++){
-                    dp[0][tar] = (tar%coins[0]==0);
-            }
-            for(int ind=1; ind<n; ind++){
-                    for(int tar=0; tar<=amount; tar++){
-                           int nottake = dp[ind-1][tar];
-              int take = 0;
-                if(coins[ind]<=tar){
-                        take = dp[ind][tar-coins[ind]];
-                }
-                 dp[ind][tar] =  nottake + take;  
-                    }
-            }
-            
-            return dp[n-1][amount];
+            vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+        return fun(n-1,amount,coins,dp);
     }
 };
