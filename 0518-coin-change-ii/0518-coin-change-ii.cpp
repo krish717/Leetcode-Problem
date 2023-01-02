a9211c3fe714f4c8fa32f71c546c2eb386222c7e
krish <61118591+krish717@users.noreply.github.com>
2023-01-02 15:28:59 +0530

    Time: 53 ms (41.34%), Space: 18.5 MB (48.89%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -14,7 +14,20 @@ public:
         }
     int change(int amount, vector<int>& coins) {
             int n = coins.size();
-            vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-        return fun(n-1,amount,coins,dp);
+            vector<vector<int>> dp(n,vector<int>(amount+1,0));
+            //Base Case
+            for(int i=0; i<=amount; i++){
+                    dp[0][i] =  (i%coins[0]==0);
+            }
+            for(int ind=1; ind<n; ind++){
+                    for(int target=0; target<=amount; target++){
+                        int notpick = dp[ind-1][target];
+                        int pick = 0;
+                        if(coins[ind]<=target)
+                        pick =  dp[ind][target-coins[ind]]; 
+                        dp[ind][target] = pick + notpick;   
+                    }
+            }
+        return dp[n-1][amount];
     }
 };
