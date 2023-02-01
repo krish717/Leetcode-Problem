1a8215c313159f7c7f6d17637566e289e9e0f42e
krish <61118591+krish717@users.noreply.github.com>
2023-02-01 12:44:43 +0530

    Time: 2 ms (49.26%), Space: 7.7 MB (80.90%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,16 +1,30 @@
 class Solution {
 public:
-    int helper(int ind,vector<int>& nums,vector<int>& dp){
-        if(ind==0) return nums[0];
-        if(ind<0) return 0;
-        if(dp[ind]!=-1) return dp[ind];
-        int first = nums[ind] + helper(ind-2,nums,dp);
-        int second = 0 + helper(ind-1,nums,dp);
-        return dp[ind] =  max(first, second);
-    }
+    // int helper(int ind,vector<int>& nums,vector<int>& dp){
+    //     if(ind==0) return nums[0];
+    //     if(ind<0) return 0;
+    //     if(dp[ind]!=-1) return dp[ind];
+    //     int first = nums[ind] + helper(ind-2,nums,dp);
+    //     int second = 0 + helper(ind-1,nums,dp);
+    //     return dp[ind] =  max(first, second);
+    // }
     int rob(vector<int>& nums) {
+        // int n = nums.size();
+        // vector<int> dp(n+1,-1);
+        // return helper(n-1,nums,dp);
+        
+        
+        //Tabulation
         int n = nums.size();
-        vector<int> dp(n+1,-1);
-        return helper(n-1,nums,dp);
+        vector<int> dp(n+1);
+        dp[0] = nums[0];
+        for(int i=1; i<n; i++){
+            int first = nums[i];
+            if(i>1)
+            first += dp[i-2];
+            int second = 0 + dp[i-1];
+            dp[i] =  max(first, second);
+        }
+        return dp[n-1];
     }
 };
