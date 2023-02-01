3ef14881ad3fe7c16641577839623b41d088e26f
krish <61118591+krish717@users.noreply.github.com>
2023-02-01 12:57:31 +0530

    Time: 5 ms (24.92%), Space: 7.7 MB (80.90%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -16,15 +16,18 @@ public:
         
         //Tabulation
         int n = nums.size();
-        vector<int> dp(n+1);
-        dp[0] = nums[0];
+        
+        int prev = nums[0];
+        int prev2 = 0;
         for(int i=1; i<n; i++){
             int first = nums[i];
             if(i>1)
-            first += dp[i-2];
-            int second = 0 + dp[i-1];
-            dp[i] =  max(first, second);
+            first += prev2;
+            int second = 0 + prev;
+            int curr =  max(first, second);
+            prev2 = prev;
+            prev = curr;
         }
-        return dp[n-1];
+        return prev;
     }
 };
