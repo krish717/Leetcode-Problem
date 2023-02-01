4494de531ddbef32dfcba33a148d8070128fbb06
krish <61118591+krish717@users.noreply.github.com>
2023-02-01 14:25:57 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (44.80%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
-        int robs(vector<int>& nums) {
-         int n = nums.size();
-         int prev2 = 0;
-            int prev = nums[0];
-            for(int i=1; i<n; i++){
-                    int pick;
-                    if(i>1){
-                            pick = nums[i] + prev2;
-                    }else{
-                            pick = nums[i];
-                    }
-                    int notpick = 0 + prev;
-                    
-                    int curr = max(pick,notpick);
-                    prev2 = prev;
-                    prev = curr;
-            }
-            return prev;
+     int houseRobber1(vector<int>& nums) {
+        //Tabulation
+        int n = nums.size();
+        
+        int prev = nums[0];
+        int prev2 = 0;
+        for(int i=1; i<n; i++){
+            int first = nums[i];
+            if(i>1)
+            first += prev2;
+            int second = 0 + prev;
+            int curr =  max(first, second);
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
     }
     int rob(vector<int>& nums) {
-            
-         int n = nums.size();
-            if(n==1) return nums[0];
-        vector<int> temp1,temp2;
-            for(int i=0; i<n; i++){
-                    if(i!=0) temp1.push_back(nums[i]);
-                    if(i!=n-1) temp2.push_back(nums[i]);
-            }
-            int ans1 = robs(temp1);
-            int ans2 = robs(temp2);
-            return max(ans1,ans2);
+        int n = nums.size();
+        if(n==1) return nums[0];
+        vector<int> arr1;
+        for(int i=1; i<n; i++){
+            arr1.push_back(nums[i]);
+        }
+        vector<int> arr2;
+        for(int i=0; i<n-1; i++){
+            arr2.push_back(nums[i]);
+        }
+        int f = houseRobber1(arr1);
+        int s = houseRobber1(arr2);
+        return max(f,s);
     }
 };
