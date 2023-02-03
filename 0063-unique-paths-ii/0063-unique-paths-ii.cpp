7de0dd3612f44de9b7f2bb861c89fb01ff7039b2
krish <61118591+krish717@users.noreply.github.com>
2023-02-03 18:46:26 +0530

    Time: 4 ms (53.92%), Space: 7.9 MB (16.03%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
-    int fun(int i,int j,vector<vector<int>>& dp,vector<vector<int>>& obstacleGrid){
-        if(i>=0 && j>=0 && obstacleGrid[i][j]==1) return 0;
-                if(i==0 && j==0) return 1;
-                if(i<0 || j<0) return 0;
+//     int fun(int i,int j,vector<vector<int>>& dp,vector<vector<int>>& obstacleGrid){
+//         if(i>=0 && j>=0 && obstacleGrid[i][j]==1) return 0;
+//                 if(i==0 && j==0) return 1;
+//                 if(i<0 || j<0) return 0;
                  
-                if(dp[i][j]!=-1) return dp[i][j];
-                int pick = fun(i-1,j,dp,obstacleGrid);
-                int notpick = fun(i,j-1,dp,obstacleGrid);
-                return dp[i][j] = pick + notpick;
-        }
+//                 if(dp[i][j]!=-1) return dp[i][j];
+//                 int pick = fun(i-1,j,dp,obstacleGrid);
+//                 int notpick = fun(i,j-1,dp,obstacleGrid);
+//                 return dp[i][j] = pick + notpick;
+//         }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
         
-        vector<vector<int>> dp(m,vector<int>(n));
-        dp[0][0] = 1;
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        // dp[0][0] = 1;
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(i>=0 && j>=0 && obstacleGrid[i][j]==1){
+                if(obstacleGrid[i][j]==1){
                     dp[i][j] = 0;
                 }else if(i==0 && j==0){
                     dp[i][j] = 1;
                 }else{
-                    int pick = fun(i-1,j,dp,obstacleGrid);
-                    int notpick = fun(i,j-1,dp,obstacleGrid);
+                    int pick = 0,notpick=0;
+                    if(i>0)
+                     pick = dp[i-1][j];
+                    if(j>0)
+                     notpick = dp[i][j-1];
                     dp[i][j] = pick + notpick;
                 }
             }
