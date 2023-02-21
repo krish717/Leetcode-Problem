70971512e2299c1d02cdaa20f320815a75a93df3
krish <61118591+krish717@users.noreply.github.com>
2023-02-21 18:04:18 +0530

    Time: 11 ms (27.22%), Space: 8.7 MB (81.66%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -5,17 +5,20 @@ public:
         int n = triangle.size();
         
         int i=0,j=0;
-        vector<vector<int>> dp(n,vector<int>(n,0));
+        vector<int> prev(n);
+        vector<int> curr(n);
         for(int j=0; j<n; j++){
-            dp[n-1][j] = triangle[n-1][j];
+            prev[j] = triangle[n-1][j];
         }
         for(int i=n-2; i>=0; i--){
+              
             for(int j=i; j>=0; j--){
-                int d = triangle[i][j] + dp[i+1][j];
-                int dg = triangle[i][j] + dp[i+1][j+1];
-                dp[i][j] = min(d,dg);
+                int d = triangle[i][j] + prev[j];
+                int dg = triangle[i][j] + prev[j+1];
+               curr[j] = min(d,dg);
             }
+            prev = curr;
         }
-        return dp[0][0];
+        return prev[0];
     }
 };
