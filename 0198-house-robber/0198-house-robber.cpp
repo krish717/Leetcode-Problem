18808a752cffc2a527cea33a74a5cda3ea0a453e
krish <61118591+krish717@users.noreply.github.com>
2023-02-21 12:22:13 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (9.54%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -10,7 +10,16 @@ public:
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n+1,-1);
-        return helper(n-1,nums,dp);
+        vector<int> dp(n+1,0);
+        // return helper(n-1,nums,dp);
+        dp[0] = nums[0];
+            for(int i=1; i<n; i++){
+                int pick = nums[i];
+                if(i>1)
+                 pick += dp[i-2];
+                int notpick = 0 + dp[i-1];
+               dp[i] = max(pick,notpick);
+            }
+        return dp[n-1];
     }
 };
