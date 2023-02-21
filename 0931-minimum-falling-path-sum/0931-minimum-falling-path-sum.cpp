d025c34022af4e9e47d2296072dbaa2a804d1fc1
krish <61118591+krish717@users.noreply.github.com>
2023-02-21 18:38:45 +0530

    Time: 18 ms (46.51%), Space: 10.2 MB (72.08%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,36 +1,23 @@
 class Solution {
 public:
+    int helper(int i,int j,vector<vector<int>>& matrix,vector<vector<int>>& dp){
+         if(j<0 || j>=matrix[0].size()) return 1e9;
+        if(i==0) return matrix[0][j];
+        if(dp[i][j]!=-1) return dp[i][j];
+         int ld = matrix[i][j] + helper(i-1,j-1,matrix,dp);
+         int u = matrix[i][j] + helper(i-1,j,matrix,dp);
+         int rd = matrix[i][j] + helper(i-1,j+1,matrix,dp);
+        return dp[i][j] = min(u,min(ld,rd));
+            
+    }
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-            vector<int> prev(n);
-            
-            
-            //base case
-            for(int j=0; j<m; j++){
-                    prev[j] = matrix[0][j];
-            }
-
-            
-            for(int i=1; i<n; i++){
-                    vector<int> curr(m);
-                    for(int j=0; j<m; j++){
-                         int up = matrix[i][j] + prev[j];
-                         int ld = matrix[i][j];
-                            if(j-1>=0) ld+=prev[j-1];
-                            else ld += 1e9;
-                         int rd = matrix[i][j];
-                            if(j+1<m) rd+=prev[j+1];
-                            else rd +=1e9; 
-                         curr[j] = min(up,min(ld,rd));   
-                    }
-                    prev = curr;
-            }
-            
-            int maxi = 1e9;
-            for(int j=0; j<m; j++){
-                    maxi = min(maxi,prev[j]);
-            }
-            return maxi;
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+        int maxi = 1e9;
+        for(int i=0; i<m; i++){
+            maxi = min(maxi,helper(n-1,i,matrix,dp));
+        }
+        return maxi;
     }
 };
