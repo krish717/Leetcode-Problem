f7e94c5c5c7441ae14254e64a6ee1c975f8a9ceb
krish <61118591+krish717@users.noreply.github.com>
2023-02-21 17:54:08 +0530

    Time: 11 ms (27.22%), Space: 9 MB (36.06%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
-    int helper(int i,int j,int n,vector<vector<int>>& triangle,vector<vector<int>>& dp){
-        if(i==n-1) return triangle[n-1][j];
-        if(dp[i][j]!=-1) return dp[i][j];
-        int d = triangle[i][j] + helper(i+1,j,n,triangle,dp);
-        int dg = triangle[i][j] + helper(i+1,j+1,n,triangle,dp);
-        return dp[i][j] = min(d,dg);
-    }
+    
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-      
+        
         int i=0,j=0;
-        vector<vector<int>> dp(n,vector<int>(n,-1));
-        return helper(i,j,n,triangle,dp);
+        vector<vector<int>> dp(n,vector<int>(n,0));
+        for(int j=0; j<n; j++){
+            dp[n-1][j] = triangle[n-1][j];
+        }
+        for(int i=n-2; i>=0; i--){
+            for(int j=i; j>=0; j--){
+                int d = triangle[i][j] + dp[i+1][j];
+                int dg = triangle[i][j] + dp[i+1][j+1];
+                dp[i][j] = min(d,dg);
+            }
+        }
+        return dp[0][0];
     }
 };
