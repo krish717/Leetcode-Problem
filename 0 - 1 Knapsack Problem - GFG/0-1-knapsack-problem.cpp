d8409c235f61534cad6a415b264f42c1b348cd39
krish <61118591+krish717@users.noreply.github.com>
2023-02-22 23:55:12 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -7,41 +7,25 @@ using namespace std;
 class Solution
 {
     public:
-    //  int fun(int ind,int W, int wt[], int val[],vector<vector<int>>& dp){
-         
-    //      if(ind==0){
-    //          if(wt[0]<=W) return val[0];
-    //          else return 0;
-    //      }
-    //      if(dp[ind][W]!=-1) return dp[ind][W];
-    //      int notpick = 0 + fun(ind-1,W,wt,val,dp);
-    //      int pick = INT_MIN;
-    //      if(wt[ind]<=W){
-    //          pick  = val[ind] + fun(ind-1,W-wt[ind],wt,val,dp);
-    //      }
-    //      return dp[ind][W] =  max(notpick,pick);
-    //  }
     //Function to return max value that can be put in knapsack of capacity W.
+    int helper(int ind,int W, int wt[], int val[], vector<vector<int>>& dp){
+        if(ind==0){
+            if(wt[0]<=W) return val[0];
+            else return 0;
+        }
+        if(dp[ind][W]!=-1) return dp[ind][W];
+        int notpick = 0 + helper(ind-1,W,wt,val,dp);
+        int pick = INT_MIN;
+        if(wt[ind]<=W)
+         pick = val[ind] + helper(ind-1,W-wt[ind],wt,val,dp);
+         
+         return dp[ind][W] = max(notpick,pick);
+    }
     int knapSack(int W, int wt[], int val[], int n) 
     { 
        // Your code here
-       vector<int> prev(W+1,0);
-       vector<int> curr(W+1,0);
-       for(int i=wt[0]; i<=W; i++){
-           prev[i] = val[0];
-       }
-       for(int ind=1; ind<n; ind++){
-           for(int weight=0; weight<=W; weight++){
-               int notpick = 0 + prev[weight];
-                int pick = INT_MIN;
-                 if(wt[ind]<=weight){
-             pick  = val[ind] + prev[weight-wt[ind]];
-         }
-         curr[weight] = max(notpick,pick);
-           }
-           prev = curr;
-       }
-       return prev[W];
+       vector<vector<int>> dp(n,vector<int>(W+1,-1));
+       return helper(n-1,W,wt,val,dp);
     }
 };
 
