e0477e62e3063487f67cb6b38d9ecde3e8f2244a
krish <61118591+krish717@users.noreply.github.com>
2023-02-23 00:26:06 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -8,24 +8,36 @@ class Solution
 {
     public:
     //Function to return max value that can be put in knapsack of capacity W.
-    int helper(int ind,int W, int wt[], int val[], vector<vector<int>>& dp){
-        if(ind==0){
-            if(wt[0]<=W) return val[0];
-            else return 0;
-        }
-        if(dp[ind][W]!=-1) return dp[ind][W];
-        int notpick = 0 + helper(ind-1,W,wt,val,dp);
-        int pick = INT_MIN;
-        if(wt[ind]<=W)
-         pick = val[ind] + helper(ind-1,W-wt[ind],wt,val,dp);
+    // int helper(int ind,int W, int wt[], int val[], vector<vector<int>>& dp){
+    //     if(ind==0){
+    //         if(wt[0]<=W) return val[0];
+    //         else return 0;
+    //     }
+    //     if(dp[ind][W]!=-1) return dp[ind][W];
+    //     int notpick = 0 + helper(ind-1,W,wt,val,dp);
+    //     int pick = INT_MIN;
+    //     if(wt[ind]<=W)
+    //      pick = val[ind] + helper(ind-1,W-wt[ind],wt,val,dp);
          
-         return dp[ind][W] = max(notpick,pick);
-    }
+    //      return dp[ind][W] = max(notpick,pick);
+    // }
     int knapSack(int W, int wt[], int val[], int n) 
     { 
        // Your code here
-       vector<vector<int>> dp(n,vector<int>(W+1,-1));
-       return helper(n-1,W,wt,val,dp);
+       vector<vector<int>> dp(n,vector<int>(W+1));
+       for(int i=wt[0]; i<=W; i++){
+           dp[0][i] = val[0];
+       }
+       for(int ind=1; ind<n; ind++){
+           for(int weight=0; weight<=W; weight++){
+                int notpick = 0 + dp[ind-1][weight];
+                int pick = INT_MIN;
+                if(wt[ind]<=weight)
+                pick = val[ind] + dp[ind-1][weight-wt[ind]];
+                dp[ind][weight] = max(notpick,pick);
+           }
+       }
+       return dp[n-1][W];
     }
 };
 
