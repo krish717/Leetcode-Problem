93b69b1d584eb178fe871c894a58f9cd4767cc84
krish <61118591+krish717@users.noreply.github.com>
2023-02-22 12:58:25 +0530

    Time: 478 ms (25.00%), Space: 94.9 MB (38.98%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,42 +1,14 @@
 class Solution {
 public:
-//    bool helper(int ind,int target,vector<int>& nums,vector<vector<bool>>& dp){
-//         if(target==0) return true;
-//         if(ind==0) return (nums[ind]==target);
-//         if(dp[ind][target]!=-1) return dp[ind][target];
-//         bool notpick = helper(ind-1,target,nums,dp);
-//         bool pick = false;
-//         if(nums[ind]<=target)
-//          pick = helper(ind-1,target-nums[ind],nums,dp);
-//         return dp[ind][target] = notpick || pick;
-//     }
-//     bool canPartition(vector<int>& nums) {
-//         int n = nums.size();
-    
-//         int totalSum = 0;
-//         for(int i=0; i<n; i++) totalSum+=nums[i];
-       
-//         int k = totalSum/2;
-//         vector<vector<bool>> dp(n,vector<bool>(k+1,-1));
-//          if(totalSum%2==1) return false;
-//         else
-//         return helper(n-1,k,nums,dp);
-//     }
-
-    bool subsetSumtoK(int i,int target,vector<int>& nums,vector<vector<int>>& dp){
+    bool helper(int ind,int target,vector<int>& nums,vector<vector<int>>& dp){
         if(target==0) return true;
-        if(i==0){
-            return nums[i]==target;
-        }
-        if(dp[i][target]!=-1) return dp[i][target];
-        
-        bool Nottaken = subsetSumtoK(i-1,target,nums,dp);
-        bool taken = false;
-        
-        if(nums[i]<=target){
-            taken = subsetSumtoK(i-1,target-nums[i],nums,dp);
-        }
-        return dp[i][target] = Nottaken || taken;
+        if(ind==0) return (nums[ind]==target);
+        if(dp[ind][target]!=-1) return dp[ind][target];
+        bool notpick = helper(ind-1,target,nums,dp);
+        bool pick = false;
+        if(nums[ind]<=target)
+         pick = helper(ind-1,target-nums[ind],nums,dp);
+        return dp[ind][target] = notpick || pick;
     }
     bool canPartition(vector<int>& nums) {
         int totalsum = 0;
@@ -49,7 +21,7 @@ public:
             int k = totalsum/2;
             int n = nums.size();
             vector<vector<int>> dp(n, vector<int>(k+1, -1));
-            return subsetSumtoK(n-1,k,nums,dp);
-        }
+            return helper(n-1,k,nums,dp);
+    }
     }
 };
