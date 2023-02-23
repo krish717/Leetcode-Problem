37adc7d448f13e7c6ee363cf6a7d4f9c74013718
krish <61118591+krish717@users.noreply.github.com>
2023-02-23 14:55:01 +0530

    Added solution - LeetHub

--- Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
+++ Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
@@ -9,23 +9,36 @@ using namespace std;
 
 class Solution{
 public:
-    int helper(int ind,int w,int val[], int wt[],vector<vector<int>>& dp){
-        if(ind==0){
-           return ((w/wt[0])*val[0]);
-        }
-        if(dp[ind][w]!=-1) return dp[ind][w];
-        int notpick = 0 + helper(ind-1,w,val,wt,dp);
-        int pick = INT_MIN;
-        if(wt[ind]<=w){
-            pick = val[ind] + helper(ind,w-wt[ind],val,wt,dp);
-        }
-        return dp[ind][w] = max(notpick,pick);
-    }
+    // int helper(int ind,int w,int val[], int wt[],vector<vector<int>>& dp){
+    //     if(ind==0){
+    //       return ((w/wt[0])*val[0]);
+    //     }
+    //     if(dp[ind][w]!=-1) return dp[ind][w];
+    //     int notpick = 0 + helper(ind-1,w,val,wt,dp);
+    //     int pick = INT_MIN;
+    //     if(wt[ind]<=w){
+    //         pick = val[ind] + helper(ind,w-wt[ind],val,wt,dp);
+    //     }
+    //     return dp[ind][w] = max(notpick,pick);
+    // }
     int knapSack(int N, int W, int val[], int wt[])
     {
         // code here
         vector<vector<int>> dp(N,vector<int>(W+1,-1));
-        return helper(N-1,W,val,wt,dp);
+        for(int i=0; i<=W; i++){
+            dp[0][i] = ((i/wt[0])*val[0]);
+        }
+        for(int ind=1; ind<N; ind++){
+            for(int weight=0; weight<=W; weight++){
+                int notpick = 0 + dp[ind-1][weight];
+        int pick = INT_MIN;
+        if(wt[ind]<=weight){
+            pick = val[ind] + dp[ind][weight-wt[ind]];
+        }
+         dp[ind][weight] = max(notpick,pick);
+            }
+        }
+        return dp[N-1][W];
     }
 };
 
