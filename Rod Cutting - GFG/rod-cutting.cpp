e5db8ddda426945a9f90ae2ee3a4635c41052354
krish <61118591+krish717@users.noreply.github.com>
2023-02-23 18:31:40 +0530

    Added solution - LeetHub

--- Rod Cutting - GFG/rod-cutting.cpp
+++ Rod Cutting - GFG/rod-cutting.cpp
@@ -10,22 +10,36 @@ using namespace std;
 
 class Solution{
   public:
-  int helper(int ind,int n,int price[],vector<vector<int>>& dp){
-      if(ind==0) return price[0]*n;
-      if(dp[ind][n]!=-1) return dp[ind][n];
-      int notpick = 0 + helper(ind-1,n,price,dp);
-      int pick = INT_MIN;
-      int rodlength = ind+1;
-      if(rodlength<=n){
-          pick = price[ind] + helper(ind,n-rodlength,price,dp);
-      }
-      return dp[ind][n] = max(notpick,pick);
-  }
+//   int helper(int ind,int n,int price[],vector<vector<int>>& dp){
+//       if(ind==0) return price[0]*n;
+//       if(dp[ind][n]!=-1) return dp[ind][n];
+//       int notpick = 0 + helper(ind-1,n,price,dp);
+//       int pick = INT_MIN;
+//       int rodlength = ind+1;
+//       if(rodlength<=n){
+//           pick = price[ind] + helper(ind,n-rodlength,price,dp);
+//       }
+//       return dp[ind][n] = max(notpick,pick);
+//   }
     int cutRod(int price[], int n) {
         //code here
   
-        vector<vector<int>> dp(n,vector<int>(n+1,-1));
-        return helper(n-1,n,price,dp);
+        vector<vector<int>> dp(n,vector<int>(n+1));
+        for(int i=0; i<=n; i++){
+            dp[0][i] = price[0]*i;
+        }
+        for(int ind=1; ind<n; ind++){
+            for(int tar=0; tar<=n; tar++){
+                int notpick = 0 + dp[ind-1][tar];
+      int pick = -1e9;
+      int rodlength = ind+1;
+      if(rodlength<=tar){
+          pick = price[ind] + dp[ind][tar-rodlength];
+      }
+       dp[ind][tar] = max(notpick,pick);
+            }
+        }
+        return dp[n-1][n];
     }
 };
 
