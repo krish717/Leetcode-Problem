bd994cce43e99da11aece9c9fb67b27d8bab0501
krish <61118591+krish717@users.noreply.github.com>
2023-02-24 11:10:23 +0530

    Time: 189 ms (40.27%), Space: 6.7 MB (90.92%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -7,19 +7,37 @@ public:
     //     return dp[ind1][ind2] = max(helper(ind1-1,ind2,s1,s2,dp),helper(ind1,ind2-1,s1,s2,dp));
     // }
     int longestPalindromeSubseq(string s) {
-        string s2 = s;
+         string s2 = s;
         reverse(s2.begin(),s2.end());
         int n = s.size();
         int m = s2.size();
-        vector<vector<int>> dp(n+1,vector<int>(m+1));
-        for(int i=0; i<=n; i++) dp[i][0] = 0;
-        for(int j=0; j<=m; j++) dp[0][j] = 0;
+        vector<int> prev(m+1),curr(m+1);
+        for(int i=0; i<=n; i++) prev[0] = 0;
+        // for(int j=0; j<=m; j++) dp[0][j] = 0;
         for(int ind1=1; ind1<=n; ind1++){
              for(int ind2=1; ind2<=m; ind2++){
-                  if(s[ind1-1]==s2[ind2-1]) dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1];
-                  else  dp[ind1][ind2] = max(dp[ind1-1][ind2],dp[ind1][ind2-1]);
+                  if(s[ind1-1]==s2[ind2-1]) curr[ind2] = 1 + prev[ind2-1];
+                  else  curr[ind2] = max(prev[ind2],curr[ind2-1]);
              }
+            prev = curr;
         }
-        return dp[n][m];
+        return prev[m];
+        
+        
+        //Tabulation
+        // string s2 = s;
+        // reverse(s2.begin(),s2.end());
+        // int n = s.size();
+        // int m = s2.size();
+        // vector<vector<int>> dp(n+1,vector<int>(m+1));
+        // for(int i=0; i<=n; i++) dp[i][0] = 0;
+        // for(int j=0; j<=m; j++) dp[0][j] = 0;
+        // for(int ind1=1; ind1<=n; ind1++){
+        //      for(int ind2=1; ind2<=m; ind2++){
+        //           if(s[ind1-1]==s2[ind2-1]) dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1];
+        //           else  dp[ind1][ind2] = max(dp[ind1-1][ind2],dp[ind1][ind2-1]);
+        //      }
+        // }
+        // return dp[n][m];
     }
 };
