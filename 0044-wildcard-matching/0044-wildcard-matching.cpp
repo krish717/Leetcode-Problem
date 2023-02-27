b42bca915148ccf565921f9c1a2565861df5a3be
krish <61118591+krish717@users.noreply.github.com>
2023-02-27 12:37:41 +0530

    Time: 119 ms (18.59%), Space: 11.2 MB (70.26%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -24,23 +24,12 @@ public:
         int m=p.size();
         vector<vector<bool>> dp(n+1,vector<bool>(m+1,false));
         dp[0][0] = true;
-        // for(int i=1; i<=n; i++){
-        //     dp[i][0] = false;
-        // }
-        // for(int j=1; j<=m; j++){
-        //     bool flag = true;
-        //      for(int k=1; k<=j; k++){
-        //         if(p[k-1]!='*'){
-        //              flag = false;
-        //             break;
-        //           }
-        //        }
-        //     dp[j][0] = flag;
-        // }
-        dp[0][0] = true;
-        for (int j = 0; j < p.size() && p[j] == '*'; ++j) {
-            dp[0][j + 1] = true;
+        for(int i=1; i<=n; i++){
+            dp[i][0] = false;
         }
+        for(int j = 1 ; j <= m ; j++)
+         dp[0][j] = dp[0][j-1] && (p[j-1] == '*');
+       
         
         for(int i=1; i<=n; i++){
             for(int j=1; j<=m; j++){
