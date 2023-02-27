b428ca273a40b28e3414215532cbda5999a38991
krish <61118591+krish717@users.noreply.github.com>
2023-02-27 11:26:32 +0530

    Time: 58 ms (67.97%), Space: 28 MB (6.83%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     bool helper(int i,int j,string &s, string &p,vector<vector<int>>& dp){
-        if(i<0 && j<0) return true;
-        if(j<0) return false;
-        if(i<0){
-            for(int k=0; k<=j; k++){
-                if(p[k]!='*') return false;
+        if(i==0 && j==0) return true;
+        if(j==0) return false;
+        if(i==0){
+            for(int k=1; k<=j; k++){
+                if(p[k-1]!='*') return false;
                }
             return true;
         } 
         if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i]==p[j] || p[j]=='?'){
+        if(s[i-1]==p[j-1] || p[j-1]=='?'){
             return dp[i][j] = helper(i-1,j-1,s,p,dp);
         }
-        if(p[j]=='*'){
+        if(p[j-1]=='*'){
             return dp[i][j] = helper(i-1,j,s,p,dp) || helper(i,j-1,s,p,dp);
         }
         return dp[i][j] = false;
@@ -37,7 +37,7 @@ public:
     bool isMatch(string s, string p) {
         int n=s.size();
         int m=p.size();
-        vector<vector<int>> dp(n,vector<int>(m+1,-1));
-        return helper(n-1,m-1,s,p,dp);
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        return helper(n,m,s,p,dp);
     }
 };
