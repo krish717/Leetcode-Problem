95b97859fbcc1bf110d16047f4045f595ad6b682
krish <61118591+krish717@users.noreply.github.com>
2023-02-28 15:24:37 +0530

    Time: 17 ms (15.28%), Space: 17.7 MB (6.23%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int helper(int ind,int buy,int n,vector<int>& prices,vector<vector<int>>& dp){
+        if(ind==n) return 0;
+        if(dp[ind][buy]!=-1) return dp[ind][buy];
+        int profit = 0;
+        if(buy==1){
+            profit = max((-prices[ind] + helper(ind+1,0,n,prices,dp)),(0 + helper(ind+1,1,n,prices,dp)));
+        }else{
+            profit = max((prices[ind] + helper(ind+1,1,n,prices,dp)),(0 + helper(ind+1,0,n,prices,dp)));
+        }
+        return dp[ind][buy] = profit;
+    }
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>> dp(n,vector<int>(2,-1));
+        return helper(0,1,n,prices,dp);
+    }
+};
