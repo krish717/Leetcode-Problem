d8b5008139927e4968319aa525d83b075b6cecc3
krish <61118591+krish717@users.noreply.github.com>
2023-03-02 17:19:06 +0530

    Time: 11 ms (87.40%), Space: 16 MB (19.90%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,20 +1,36 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int,int> um;
-        int n = nums.size(); 
+        int nums1=-1,nums2=-1,c1=0,c2=0;
+        int n = nums.size();
+        int k = n/3;
         for(int i=0; i<n; i++){
-            um[nums[i]]++;
-        }
-        vector<int> v;
-        int s = n/3;
-         for(int i=0; i<n; i++){
-            if(um.find(nums[i])!=um.end()){
-                if(um[nums[i]]>s)
-                v.push_back(nums[i]);
-                um.erase(nums[i]);
+            if(nums1==nums[i]) c1++;
+            else if(nums2==nums[i]) c2++;
+            else if(c1==0){
+                nums1 = nums[i];
+                c1=1;
+            }else if(c2==0){
+                nums2 = nums[i];
+                c2=1;
+            }else{
+                c1--;
+                c2--;
             }
         }
+        c1=0,c2=0;
+        for(int i=0; i<n; i++){
+            if(nums1==nums[i]) c1++;
+             else if(nums2==nums[i]) c2++;
+        }
+        vector<int> v;
+        if(c1>k) v.push_back(nums1);
+         if(c2>k) v.push_back(nums2);
         return v;
+    
+    // vector<int> res;
+    // if(c1 > k) res.push_back(nums1);
+    // if(c2 > k) res.push_back(nums2);
+    // return res;
     }
 };
