18f298c94c4114dcbb6e506c5b94ded648960a0e
krish <61118591+krish717@users.noreply.github.com>
2023-03-02 12:18:42 +0530

    Time: 657 ms (27.70%), Space: 292.3 MB (9.65%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -11,7 +11,21 @@ public:
         return dp[ind][prev_ind+1] = max(notpick,pick);
     }
     int lengthOfLIS(vector<int>& nums) {
-        vector<vector<int>> dp(nums.size(),vector<int>(nums.size()+1,-1));
-        return helper(0,-1,nums,dp);
+        int n = nums.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        // return helper(0,-1,nums,dp);
+        
+        //Tabulation
+        for(int ind=n-1; ind>=0; ind--){
+            for(int prev_ind=ind-1; prev_ind>=-1; prev_ind--){
+                int notpick = 0 + dp[ind+1][prev_ind+1];
+        int pick = -1e9;
+        if(prev_ind==-1 || nums[ind]>nums[prev_ind]){
+            pick = 1 + dp[ind+1][ind+1];
+        }
+         dp[ind][prev_ind+1] = max(notpick,pick);
+            }
+        }
+        return dp[0][-1+1];
     }
 };
