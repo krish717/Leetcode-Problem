c3bf464ea87bb152443de0f321a2d5f74f827573
krish <61118591+krish717@users.noreply.github.com>
2023-03-04 12:37:48 +0530

    Time: 480 ms (33.51%), Space: 10.7 MB (38.33%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -11,21 +11,40 @@ public:
     //     return dp[ind][prev_ind+1] = max(notpick,pick);
     // }
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+//         int n = nums.size();
+//         vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+//         // return helper(0,-1,nums,dp);
+        
+//         //Tabulation
+//         for(int ind=n-1; ind>=0; ind--){
+//             for(int prev_ind=ind-1; prev_ind>=-1; prev_ind--){
+//                 int notpick = 0 + dp[ind+1][prev_ind+1];
+//         int pick = -1e9;
+//         if(prev_ind==-1 || nums[ind]>nums[prev_ind]){
+//             pick = 1 + dp[ind+1][ind+1];
+//         }
+//          dp[ind][prev_ind+1] = max(notpick,pick);
+//             }
+//         }
+//         return dp[0][-1+1];
+        
+        //Space optimize
+         int n = nums.size();
+        vector<int> prev(n+1,0),curr(n+1,0);
         // return helper(0,-1,nums,dp);
         
         //Tabulation
         for(int ind=n-1; ind>=0; ind--){
             for(int prev_ind=ind-1; prev_ind>=-1; prev_ind--){
-                int notpick = 0 + dp[ind+1][prev_ind+1];
-        int pick = -1e9;
-        if(prev_ind==-1 || nums[ind]>nums[prev_ind]){
-            pick = 1 + dp[ind+1][ind+1];
-        }
-         dp[ind][prev_ind+1] = max(notpick,pick);
+                int notpick = 0 + prev[prev_ind+1];
+                int pick = -1e9;
+                if(prev_ind==-1 || nums[ind]>nums[prev_ind]){
+                pick = 1 + prev[ind+1];
+                }
+                curr[prev_ind+1] = max(notpick,pick);
             }
+            prev=curr;
         }
-        return dp[0][-1+1];
+        return curr[-1+1];
     }
 };
