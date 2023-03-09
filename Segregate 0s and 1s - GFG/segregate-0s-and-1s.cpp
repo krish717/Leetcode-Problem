dcf9dc1c30b859afc601a047bcd4e00fc8a03d8a
krish <61118591+krish717@users.noreply.github.com>
2023-03-09 17:13:54 +0530

    Added solution - LeetHub

--- Segregate 0s and 1s - GFG/segregate-0s-and-1s.cpp
+++ Segregate 0s and 1s - GFG/segregate-0s-and-1s.cpp
@@ -11,15 +11,22 @@ class Solution{
 public:
     void segregate0and1(int arr[], int n) {
         // code here
-        int count0 = 0;
-        for(int i=0; i<n; i++){
-            if(arr[i]==0) count0++;
-        }
-        for(int i=0; i<count0; i++){
-            arr[i] = 0;
-        }
-        for(int i=count0; i<n; i++){
-            arr[i] = 1;
+        // int count0 = 0;
+        // for(int i=0; i<n; i++){
+        //     if(arr[i]==0) count0++;
+        // }
+        // for(int i=0; i<count0; i++){
+        //     arr[i] = 0;
+        // }
+        // for(int i=count0; i<n; i++){
+        //     arr[i] = 1;
+        // }
+        
+        int i=0; int j=n-1;
+        while(i<j){
+           while(i<j && arr[i]==0) i++;
+           while(i<j && arr[j]==1) j--;
+           if(i<j) swap(arr[i++],arr[j--]);
         }
         
         
