1ad8adbf5cf9afec024b79fca94822bc79912b9c
krish <61118591+krish717@users.noreply.github.com>
2023-03-25 12:32:43 +0530

    Time: 12 ms (63.92%), Space: 19.1 MB (10.44%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-//         unordered_map<int, bool> umpp;
-//             for(int i=0; i<nums.size(); i++){
-//                     umpp[nums[i]] = true;
-//             }
-//             int k = 0;
-            
-//             for(auto it:nums){
-//                     if(umpp.find(it)!=umpp.end()){
-//                             nums[k++] = it;
-//                             umpp.erase(it);
-//                     }
-//             }
-//             return k;
-            if(nums.size()==1) return 1;
-            int j=1;
-            for(int i=1; i<nums.size(); i++){
-                    if(nums[i]!=nums[i-1]){
-                            nums[j++] = nums[i];
-                    }
+        unordered_map<int, int> um;
+        for(int i=0; i<nums.size(); i++){
+            um[nums[i]] = 1;
+        }
+        int count=0;
+        int k=0;
+        for(int i=0; i<nums.size(); i++){
+            if(um.count(nums[i])>0){
+                count++;
+                nums[k++] = nums[i];
+                um.erase(nums[i]);
             }
-            return j;
+        }
+        return count;
     }
 };
