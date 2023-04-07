c0bfe362b5e8f0cc98aa5026dcafe44178206b90
krish <61118591+krish717@users.noreply.github.com>
2023-04-07 10:41:18 +0530

    Time: 38 ms (71.88%), Space: 15.4 MB (51.69%) - LeetHub

--- 2414-length-of-the-longest-alphabetical-continuous-substring/2414-length-of-the-longest-alphabetical-continuous-substring.cpp
+++ 2414-length-of-the-longest-alphabetical-continuous-substring/2414-length-of-the-longest-alphabetical-continuous-substring.cpp
@@ -1,28 +1,15 @@
 class Solution {
 public:
     int longestContinuousSubstring(string s) {
-         vector<int> nums;
-    for(int i=0; i<s.size(); i++){
-        char myChar = s[i];
-        int myInt = myChar - '0';
-        nums.push_back(myInt);
-    }
-         // sort(nums.begin(),nums.end());
-        int n = nums.size();
-        if(n==0) return 0;
-        int longest = 1;
-        int lastSmaller = INT_MIN;
-        int count=0;
-        for(int i=0; i<n; i++){
-            if(nums[i]-1==lastSmaller){
-                count++;
-                lastSmaller = nums[i];
-            }else if(nums[i]!=lastSmaller){
+        int ans=0,count=1;
+        for(int i=1; i<s.size(); i++){
+            if(s[i-1]+1==s[i]) count++;
+            else{
+                ans = max(count,ans);
                 count=1;
-                lastSmaller = nums[i];
             }
-            longest = max(longest,count);
         }
-        return longest;
+        ans = max(ans,count);
+        return ans;
     }
 };
