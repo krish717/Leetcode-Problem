1107cb09a3197126604e9f3cfbb136b7d197ab33
krish <61118591+krish717@users.noreply.github.com>
2023-04-09 15:46:41 +0530

    Time: 137 ms (66.16%), Space: 57.2 MB (89.07%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        for(int i=0; i<nums.size(); i++){
+            if(i+1<nums.size() && nums[i]==nums[i+1]) return true;
+        }
+        return false;
+    }
+};
