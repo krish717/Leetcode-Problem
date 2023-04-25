37597984584783559f61ed8de90ede5a543d8df4
krish <61118591+krish717@users.noreply.github.com>
2023-04-25 19:28:05 +0530

    Added solution - LeetHub

--- Second Largest - GFG/second-largest.cpp
+++ Second Largest - GFG/second-largest.cpp
@@ -14,7 +14,7 @@ public:
 	    int first=-1,second=-1;
 	    for(int i=0; i<n; i++){
 	        if(arr[i]>first){
-	             second = first;
+	            second=first;
 	            first = arr[i];
 	        }else if(arr[i]>second && arr[i]!=first){
 	            second = arr[i];
