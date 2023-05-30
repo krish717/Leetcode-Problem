84637d18d1089ad6c027b230fbe4fd0fbcc0c77c
krish <61118591+krish717@users.noreply.github.com>
2023-05-30 11:41:08 +0530

    Added solution - LeetHub

--- Union of Two Sorted Arrays - GFG/union-of-two-sorted-arrays.cpp
+++ Union of Two Sorted Arrays - GFG/union-of-two-sorted-arrays.cpp
@@ -11,16 +11,35 @@ class Solution{
     //Function to return a list containing the union of the two arrays. 
     vector<int> findUnion(int arr1[], int arr2[], int n, int m)
     {
-        set<int> s;
-        for(int i=0; i<n; i++){
-            s.insert(arr1[i]);
-        }
-         for(int i=0; i<m; i++){
-            s.insert(arr2[i]);
-        }
+        //Your code here
+        //return vector with correct order of elements
         vector<int> v;
-         for(auto it:s){
-            v.push_back(it);
+        int i=0,j=0;
+        while(i<n && j<m){
+            if(arr1[i]<=arr2[j]){
+                if(v.size()==0 || v.back()!=arr1[i]){
+                    v.push_back(arr1[i]);
+                }
+                i++;
+            }else{
+                if(v.size()==0 || v.back()!=arr2[j]){
+                    v.push_back(arr2[j]);
+                }
+                j++;
+            }
+        }
+        
+        while(i<n){
+            if(v.size()==0 || v.back()!=arr1[i]){
+                    v.push_back(arr1[i]);
+                }
+                i++;
+        }
+        while(j<m){
+            if(v.size()==0 || v.back()!=arr2[j]){
+                    v.push_back(arr2[j]);
+                }
+                j++;
         }
         return v;
     }
